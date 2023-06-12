7c7b89bbbc633d0f536422cd9d9920ed3e2cbd51
jruizsilva <ruizsilvajonathan@gmail.com>
2023-06-12 10:23:20 -0300

    model user updated

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,6 @@
 import { Schema, Types, model, Model } from 'mongoose'
+import bcrypt from 'bcrypt'
+
 import { User } from '../interfaces/user'
 
 const UserSchema = new Schema<User>(
@@ -14,6 +16,7 @@ const UserSchema = new Schema<User>(
     email: {
       type: String,
       required: true,
+      lowercase: true,
       unique: true
     },
     password: {
@@ -26,6 +29,29 @@ const UserSchema = new Schema<User>(
     versionKey: false
   }
 )
+
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(this.password, salt)
+    this.password = hashedPassword
+
+    next()
+  } catch (error: any) {
+    next(error)
+  }
+})
+
+UserSchema.methods.isValidPassword = async function (
+  password: string
+) {
+  try {
+    console.log(this.id)
+    return await bcrypt.compare(password, this.password)
+  } catch (error) {
+    throw error
+  }
+}
 
 const UserModel = model('users', UserSchema)
 
